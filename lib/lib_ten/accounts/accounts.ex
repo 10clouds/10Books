d83d4ae950cb602af79d04ac2c52f08b32933bcd
Ruslan <ruslan.savenok@gmail.com>
2017-10-08 01:37:23 +0300

    find_or_create_user method

--- lib/lib_ten/accounts/accounts.ex
+++ lib/lib_ten/accounts/accounts.ex
@@ -13,4 +13,17 @@ defmodule LibTen.Accounts do
     |> User.changeset(attrs)
     |> Repo.insert()
   end
+
+  def find_or_create_user(attrs \\ %{}) do
+    if String.ends_with?(attrs.email, "@10clouds.com") do
+      user = Repo.get_by(User, %{email: attrs.email})
+      if user do
+        {:ok, user}
+      else
+        create_user(attrs)
+      end
+    else
+      {:error, :invalid_email_domain}
+    end
+  end
 end
--- test/lib_ten/accounts/accounts_test.exs
+++ test/lib_ten/accounts/accounts_test.exs
@@ -21,4 +21,25 @@ defmodule LibTen.AccountsTest do
       assert user == Accounts.get_by!(%{email: user.email})
     end
   end
+
+  describe "users -> find_or_create_user/1" do
+    test "returns an error if email is not @10clouds.com" do
+      user = %{email: "test@test.com"}
+      assert {:error, :invalid_email_domain} = Accounts.find_or_create_user(user)
+    end
+
+    test "creates user if it is not present" do
+      assert_raise Ecto.NoResultsError,
+        fn -> Accounts.get_by!(%{email: @valid_attrs.email}) end
+      assert {:ok, user} = Accounts.find_or_create_user(@valid_attrs)
+      assert user.email == @valid_attrs.email
+      assert user.name == @valid_attrs.name
+    end
+
+    test "returns exising user if it is present" do
+      exising_user = user_fixture(%{email: "exising-user@10clouds.com"})
+      assert {:ok, user} = Accounts.find_or_create_user(exising_user)
+      assert user == exising_user
+    end
+  end
 end
