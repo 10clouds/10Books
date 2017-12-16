89480852bbdaa10eb8b980f1094c478aec185fb1
Ruslan <ruslan.savenok@gmail.com>
2017-12-16 19:19:01 +0100

    Do not try to connect to web socket if window.currentUserToken not present

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -6,7 +6,9 @@ const socket = new Socket('/socket', {
   }
 });
 
-socket.connect();
+if (window.currentUserToken) {
+  socket.connect();
+}
 
 export const productsChannel = socket.channel('products');
 export const categoriesChannel = socket.channel('categories');
