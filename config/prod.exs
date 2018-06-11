24c4c52a14bd9d6d9f5ccc42d7a93f3ff4a8b076
Ruslan Savenok <ruslan.savenok@gmail.com>
2018-06-11 14:02:00 +0200

    Update mail

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :lib_ten, LibTenWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "books.10clouds.com", port: 4000],
+  url: [host: "books.10clouds.com"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   code_reloader: false
