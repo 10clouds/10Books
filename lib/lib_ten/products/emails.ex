377dfa1ecdf1be6dfab499e7abe287d081c2e6da
Ruslan Savenok <ruslan.savenok@gmail.com>
2018-06-11 13:40:16 +0200

    Fix email

--- lib/lib_ten/products/emails.ex
+++ lib/lib_ten/products/emails.ex
@@ -6,14 +6,14 @@ defmodule LibTen.Products.Emails do
   alias LibTen.Products.Product
   alias LibTen.Accounts.User
 
-  @default_from "10Books <no-reply@10clouds.com>"
+  @default_from "no-reply@10clouds.com"
   @default_endpoint LibTenWeb.Endpoint
 
   def product_has_been_returned(%Product{} = product, %User{} = user) do
     new_email()
     |> to(user.email)
     |> from(@default_from)
-    |> subject("📚 \"#{product.title}\" is now available")
+    |> subject("10Books: \"#{product.title}\" is now available")
     |> render("product_has_been_returned.html", %{
       product: product,
       conn: @default_endpoint
@@ -24,7 +24,7 @@ defmodule LibTen.Products.Emails do
     new_email()
     |> to(product.used_by.user.email)
     |> from(@default_from)
-    |> subject("🚨🚨 Please return \"#{product.title}\" 🚨🚨")
+    |> subject("10Books: 🚨🚨 Please return \"#{product.title}\" 🚨🚨")
     |> render("request_product_return.html", %{
       product: product,
       return_subscribers_count: length(product.used_by.return_subscribers)
