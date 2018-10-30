f0c00245d853b7924e77f4428a816331100c6d08
Ruslan Savenok <ruslan.savenok@gmail.com>
2018-10-30 09:08:40 +0100

    Phoenix version update

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule LibTen.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.3.4"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:phoenix_active_link, "~> 0.1.1"},
@@ -45,7 +45,7 @@ defmodule LibTen.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:ueberauth_google, "~> 0.5"},
       {:ex_machina, "~> 2.1", only: :test},
       {:mock, "~> 0.2.0", only: :test},
