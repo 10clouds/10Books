1591b9cae9e73211a056fcea5220751d89c9794a
Ruslan Saven <ruslan.savenok@gmail.com>
2019-09-20 23:46:42 +0200

    Experimenting with logo sizes

--- lib/lib_ten/admin/site_logo.ex
+++ lib/lib_ten/admin/site_logo.ex
@@ -2,6 +2,13 @@ defmodule LibTen.Admin.SiteLogo do
   use Arc.Definition
   use Arc.Ecto.Definition
 
+  @extension_whitelist ~w(.jpg .jpeg .gif .png .svg)
+
+  def validate({file, _}) do
+    file_extension = file.file_name |> Path.extname |> String.downcase
+    Enum.member?(@extension_whitelist, file_extension)
+  end
+
   def filename(_, _) do
     "admin/site_logo"
   end
